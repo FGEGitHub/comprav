54f088a2b8cfdb5e988c78624fdaec9d73084466
FGEGitHub <pipao.pipo@gmail.com>
2025-07-06 12:03:00 -0300

    rutas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,7 @@ import Adminstock from '../pages/admin/stock/index';
 import AdminProducts from '../pages/admin/listaproductos/index';
 import React from 'react';
 const Rutas = [
+      { path: '/', element: <Inicio /> },
   { path: '/inicio', element: <Inicio /> },
   { path: '/admin/stock', element: <Adminstock /> },
   { path: '/admin/productos', element: <AdminProducts /> }
